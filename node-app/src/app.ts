184b60de21daff9020fe7dd6412cc82914e12e3e
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-07 16:22:15 +0900

    cssを分ける・ルーティングをする

--- node-app/src/app.ts
+++ node-app/src/app.ts
@@ -1,5 +1,10 @@
 import http from "node:http"; // httpを使えるようにするよ
 import pug from 'pug';
+import url from 'node:url'
+import fs from 'node:fs/promises'
+
+const index_template = pug.compileFile('./index.pug') // ファイルはここで読み込む
+const style_css = await fs.readFile('style.css', 'utf8')
 
 const server = http.createServer(getFromClient)
 
@@ -10,11 +15,40 @@ console.log('Server start!') // サーバー側に出力される
 
 // createServer の処理
 async function getFromClient(req: http.IncomingMessage, res: http.ServerResponse) {
-    const content = pug.renderFile('./index.pug', { // 渡す引数が複数個あるので{}でオブジェクト型として渡す
+    const url_parts = new url.URL(req.url || '', 'http://localhost:3210')
+
+    switch (url_parts.pathname) {
+        case '/': {
+            // Index(トップページにアクセスが来た時
+            const content = index_template({
+                title: 'Indexページ',
+                content: 'これはテンプレートを使ったシンプルなサンプルページです。'
+            })
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+            res.write(content)
+            res.end()
+            break
+        }
+
+        case '/style.css': {
+            // スタイルシート(style.css)にアクセスが来た時
+            res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'})
+            res.write(style_css)
+            res.end()
+            break
+        }
+
+        default:
+            // 想定していないパスへのアクセス
+            res.writeHead(404,{'Content-Type': 'text/plain'})
+            res.end('no page...')
+            break
+    }
+
+    /* このプログラムだと毎回ファイルを読み込んでだるいので関数の外で読み込む方法の方が良い
+     const content = pug.renderFile('./index.pug', { // 渡す引数が複数個あるので{}でオブジェクト型として渡す
         title: 'Indexページ',
         content: 'これはテンプレートを使ったシンプルなサンプルページです。'
     })
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-    res.write(content)
-    res.end()
+    */
 }
