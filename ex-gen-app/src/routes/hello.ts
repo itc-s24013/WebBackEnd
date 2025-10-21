e24d999d167d2a743f087192d05c86648e8c36a9
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-21 15:04:58 +0900

    getを行った時も最後に送ったメッセージが表示されるようにした

--- ex-gen-app/src/routes/hello.ts
+++ ex-gen-app/src/routes/hello.ts
@@ -1,17 +1,28 @@
-import { Router } from 'express'
+import express, { Router } from 'express'
 
 const router = Router()
 
+declare module 'express-session' {
+    interface SessionData {
+        message?: string
+    }
+}
+
 router.get('/', async (req, res, next) => {
+    const msg = req.session.message !== undefined
+        ? `Last Message: ${req.session.message}`
+        : '※何か書いて送信してください。'
+    // 条件式 ? 真の時の値 : 偽の時の値
     const data = {
         title: 'Hello',
-        content: '※なにか書いて送信してください'
+        content: msg
     }
     res.render('hello', data)
 })
 
 router.post('/post', async (req, res, next) => {
     const msg = req.body.message as string | undefined
+    req.session.message = msg
     const data = {
         title: 'Hello',
         content: `あなたは、「${msg}」と送信しました。`
