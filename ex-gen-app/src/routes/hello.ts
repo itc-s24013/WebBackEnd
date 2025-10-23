1135cc1196f30a6a4bcbd9ae11fbdb7daaa5c1e2
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-23 15:34:24 +0900

    idで指定して更新処理を行う

--- ex-gen-app/src/routes/hello.ts
+++ ex-gen-app/src/routes/hello.ts
@@ -43,15 +43,34 @@ router.post('/add', async (req, res, next) => {
 router.get('/show', async (req, res, next) => {
     const id = Number(req.query.id)
     const result: MyData[] = await db.query(
-        'SELECT * FROM mydata WHERE id = ?',[
-            id
-        ])
+        'SELECT * FROM mydata WHERE id = ?',[id])
     res.render('hello/show', {
         title: 'Hello/show',
         content: `id=${id} のレコード`,
         mydata: result[0]
         }
     )
+})
+
+router.get('/edit', async (req, res, next) => {
+    const id = Number(req.query.id)
+    const result: MyData[] = await db.query(
+        'SELECT * FROM mydata WHERE id = ?',[id]
+    )
+    res.render('hello/edit', {
+        title: 'Hello/edit',
+        content: `id=${id} のレコード`,
+        mydata: result[0]
+    })
+})
+
+router.post('/edit', async (req, res, next) => {
+    const {id, name, mail, age} = req.body
+    await db.query(
+        'UPDATE mydata SET name=?, mail=?, age=? WHERE id = ?',[
+            name, mail, age, id
+        ])
+    res.redirect('/hello')
 })
 
 export default router
