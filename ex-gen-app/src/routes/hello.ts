73e6979b82c0fcfb0b55bccf15d136297569c01f
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-28 14:36:12 +0900

    エラーメッセージの受取と値のチェックを行う

--- ex-gen-app/src/routes/hello.ts
+++ ex-gen-app/src/routes/hello.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express'
 import mariadb from 'mariadb'
+import {check, validationResult} from "express-validator";
 
 const router = Router()
 const db = await mariadb.createConnection({
@@ -28,17 +29,34 @@ router.get('/', async (req, res, next) => {
 router.get('/add', async (req, res, next) => {
     res.render('hello/add', {
         title: 'Hello/Add',
-        content: '新しいレコードを入力'
+        content: '新しいレコードを入力',
+        form: {name: '', mail: '', age: 0},
+        error: {}
     })
 })
 
-router.post('/add', async (req, res, next) => {
-    const { name, mail, age } = req.body
-    await db.query('INSERT INTO mydata (name, mail, age) VALUES (?,?,?)', [
-        name, mail, age
-    ])
-    res.redirect('/hello')
-})
+router.post('/add',
+    check('name', 'NAME は必ず入力してください').notEmpty(),
+    check('mail','MAIL はメールアドレスを入力してください').isEmail(),
+    check('age','AGE は年齢(整数)を入力してください').isInt(),
+    async (req, res, next) => {
+        const result = validationResult(req)
+        if (!result.isEmpty()) { // エラーが出た時(エラーが出てたら空になる)
+            // 入力チェックに引っかかったら再レンダリング
+            res.render('hello/add', {
+                title: 'Hello/Add',
+                content: '新しいレコードを入力',
+                form: req.body,
+                error: result.mapped()
+            })
+            return
+        }
+        const { name, mail, age } = req.body
+        await db.query('INSERT INTO mydata (name, mail, age) VALUES (?,?,?)', [
+            name, mail, age
+        ])
+        res.redirect('/hello')
+    })
 
 router.get('/show', async (req, res, next) => {
     const id = Number(req.query.id)
