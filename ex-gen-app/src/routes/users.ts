e4b26a7abe3ae960f1c917ee286e836e4b8bc38c
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-29 15:38:17 +0900

    where句の実装

---
+++ ex-gen-app/src/routes/users.ts
@@ -0,0 +1,31 @@
+import {Router, Request} from 'express'
+import {PrismaMariaDb} from '@prisma/adapter-mariadb'
+import {PrismaClient} from 'db'
+
+const router = Router()
+const adapter = new PrismaMariaDb({
+    host: 'localhost',
+    port: 3306,
+    user: 'prisma',
+    password: 'prisma',
+    database: 'chap6',
+    connectionLimit: 5
+})
+const prisma = new PrismaClient({adapter})
+
+interface UserPrams {
+    id?: string
+}
+
+router.get('/', async (req: Request<{}, {}, {}, UserPrams>, res, next) => {
+    const id = parseInt(req.query.id || '')
+    const users = await (id ? prisma.user.findMany({where: {id}})
+        : prisma.user.findMany())
+
+    res.render('users/index', {
+        title: 'Users/Index',
+        content: users,
+    })
+})
+
+export default router
